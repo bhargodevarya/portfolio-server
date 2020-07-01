54653c9f26121bb4eaa6154d8f2f65fe15384b3d
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-01 19:32:09 +0530

    Adding route for portfolio endpoint

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const server = express();
+const portfolioRoutes = require('./routes/portfolios')
 
 let PORT = 3001
 
 server.get('/health', (req, res) => {
     return res.json({status: 'Up'})
 })
+
+
+server.use('/api/v1', portfolioRoutes);
 
 server.listen(PORT, err => {
     if (err) console.error("Error while starting server")
