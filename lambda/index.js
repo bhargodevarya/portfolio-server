b182e6cff15d653d16cfaa74ef213b491f9f6adf
Bhargodev Arya <bhargodevarya@gmail.com>
2020-08-23 16:47:25 +0530

    Lambda to read the json file from S3

---
+++ lambda/index.js
@@ -0,0 +1,35 @@
+// Load the AWS SDK for Node.js
+var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "us-east-1" });
+
+// Create S3 service object
+//s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+
+  var bucketParams = {
+    Bucket: "portfolio-data-bucket",
+    Key: "portfolio.json",
+  };
+
+  var s3Response = await s3.getObject(bucketParams).promise();
+
+  // Call S3 to obtain a list of the objects in the bucket
+//   await s3.getObject(bucketParams, function (err, data) {
+//     if (err) {
+//       console.log("Error", err);
+//     } else {
+//       console.log("Success", data.Body.toString());
+//       s3Response = data.Body.toString()
+//     }
+//   });
+  console.log("Returning response", s3Response)
+  return s3Response.Body.toString();
+};
